a183ca1a2fccdc3ab33ccfcdceeb907d22c21d6d
sinuabe96 <sinuabebe96@gmail.com>
2022-12-08 09:31:01 -0800

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *main - Entry Ponit
+ *
+ * Retrun: Always 0 (success)
+ */
+int main(void)
+{
+	puts("\"programming is like buliding a multilingual puzzle");
+	return (0);
+
+}
